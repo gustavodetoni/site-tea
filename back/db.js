db96c7522274b05845d35066536886f89422de80
gustavodetoni <gustavodetoni2407@gmail.com>
2025-03-13 22:16:47 -0300

    fix db connect

--- back/db.js
+++ back/db.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2"
 
-export const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "root",
-    database: process.env.DB_NAME || "tea"
+export const db = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "root",
+  database: process.env.DB_NAME || "tea",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect((err) => {
